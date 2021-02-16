5c9a1c4b846e610d75ca47ce3bd4d2b6359aaa64
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2021-02-16 11:10:04 +0000

    Update app.js
    hid new game button when the game starts and made it visible when game is won.

--- app.js
+++ app.js
@@ -1,102 +1,102 @@
 (function starter(){
 
-var roundScore, player, currentScore;
-let randomTotal;
-globalScore = [0, 0];
-player = 0;
-currentScore = 0;
-
-let setScorebtn = document.querySelector('#play-btn');
-let input = document.querySelector('#input');
-let inputValue;
-let setBtn = document.getElementById('play-btn');
-let rollBtn = document.getElementById('roll-btn');
-let holdBtn = document.getElementById('hold-btn');
-let newBtn = document.getElementById('new-btn');
-let img = document.getElementById('dice-img');
-img.classList.add('hide');
-let img1 = document.getElementById('dice-img-1');
-img1.classList.add('hide');
-
-initial();
-
-rollBtn.addEventListener('click', function(){
-    let random = Math.floor(Math.random() * 6) + 1;
-    let random1 = Math.floor(Math.random() * 6) + 1;
-    randomTotal = random + random1;
-    img.src = 'dice-' + random + '.png';
-    img.classList.remove('hide');
-    img1.src = 'dice-' + random1 + '.png';
-    img1.classList.remove('hide');
-        if(random !== 1 && random1 !== 1){
-        currentScore += randomTotal;
-        document.getElementById('current-score-' + player).textContent = currentScore;
+    var roundScore, player, currentScore, randomTotal,         globalScore;
+    
+        globalScore = [0, 0];
+        player = 0;
+        currentScore = 0;
+    
+    let setScorebtn = document.querySelector('#play-btn');
+    let input = document.querySelector('#input');
+    let inputValue;
+    let setBtn = document.getElementById('play-btn');
+    let rollBtn = document.getElementById('roll-btn');
+    let holdBtn = document.getElementById('hold-btn');
+    let newBtn = document.getElementById('new-btn');
+    let img = document.getElementById('dice-img');
+    img.classList.add('hide');
+    let img1 = document.getElementById('dice-img-1');
+    img1.classList.add('hide');
+    
+    initial();
+    
+    rollBtn.addEventListener('click', function(){
+        let random = Math.floor(Math.random() * 6) + 1;
+        let random1 = Math.floor(Math.random() * 6) + 1;
+        randomTotal = random + random1;
+        img.src = 'dice-' + random + '.png';
+        img.classList.remove('hide');
+        img1.src = 'dice-' + random1 + '.png';
+        img1.classList.remove('hide');
+            if(random !== 1 && random1 !== 1){
+            currentScore += randomTotal;
+            document.getElementById('current-score-' + player).textContent = currentScore;
+            }else{
+            nextPlayer();
+        } 
+    });
+    
+    holdBtn.addEventListener('click', function(){
+        globalScore[player] += currentScore;
+        document.getElementById('global-' + player).textContent = globalScore[player];
+        if(globalScore[player] < inputValue){
+            nextPlayer();
         }else{
+            holdBtn.classList.add('hide');
+            rollBtn.classList.add('hide');
+            newBtn.classList.remove('hide')
+            document.getElementById('player-' + player).textContent = 'WINNER !';
+            document.getElementById('player-' + player).classList.add('winner');
+            img.classList.add('hide');
+            img1.classList.add('hide');
+            setBtn.classList.add('hide');
+            
+        }  
+    });
+    
+    newBtn.addEventListener('click', function(){
+        initial();
+        document.getElementById('player-0').textContent = 'PLAYER 1';
+        document.getElementById('player-1').textContent = 'PLAYER 2';
+        randomTotal = 0;
+        currentScore = 0;
+        inputValue = 0;
+        globalScore[player] = 0;
+        input.value = null;
+        setBtn.classList.remove('hide');
         nextPlayer();
-    } 
-});
-
-holdBtn.addEventListener('click', function(){
-    globalScore[player] += currentScore;
-    document.getElementById('global-' + player).textContent = globalScore[player];
-    if(globalScore[player] < inputValue){
-        nextPlayer();
-    }else{
+        
+        
+    })
+    
+    
+    function nextPlayer(){
+            document.getElementById('current-score-' + player).textContent = '0';
+            
+            player === 0 ? player = 1 : player = 0;
+            document.querySelector('#panel-0').classList.toggle('active');
+            document.querySelector('#panel-1').classList.toggle('active');
+            
+            document.getElementById('current-score-' + player).textContent = '0';
+            currentScore = 0;
+    }
+    
+    function initial(){
+        document.getElementById('global-0').textContent = '0';
+        document.getElementById('global-1').textContent = '0';
+        document.getElementById('current-score-0').textContent = '0';
+        document.getElementById('current-score-1').textContent = '0';
         holdBtn.classList.add('hide');
         rollBtn.classList.add('hide');
-        document.getElementById('player-' + player).textContent = 'WINNER !';
-        document.getElementById('player-' + player).classList.add('winner');
-        img.classList.add('hide');
-        img1.classList.add('hide');
-        setBtn.classList.add('hide');
+    };
+    
+    
+    setScorebtn.addEventListener('click', function(){
+        inputValue = input.value;
+        holdBtn.classList.remove('hide');
+        rollBtn.classList.remove('hide');
+        newBtn.classList.add('hide')
+        document.getElementById('player-' + player).classList.remove('winner');
         
-    }   
-});
-
-newBtn.addEventListener('click', function(){
-    initial();
-    document.getElementById('player-0').textContent = 'PLAYER 1';
-    document.getElementById('player-1').textContent = 'PLAYER 2';
-    randomTotal = 0;
-    currentScore = 0;
-    inputValue = 0;
-    globalScore[player] = 0;
-    input.value = null;
-    setBtn.classList.remove('hide');
-    nextPlayer();
-    
-    
-})
-
-
-function nextPlayer(){
-        document.getElementById('current-score-' + player).textContent = '0';
-        
-        player === 0 ? player = 1 : player = 0;
-        document.querySelector('#panel-0').classList.toggle('active');
-        document.querySelector('#panel-1').classList.toggle('active');
-        
-        document.getElementById('current-score-' + player).textContent = '0';
-        currentScore = 0;
-}
-
-function initial(){
-    document.getElementById('global-0').textContent = '0';
-    document.getElementById('global-1').textContent = '0';
-    document.getElementById('current-score-0').textContent = '0';
-    document.getElementById('current-score-1').textContent = '0';
-    holdBtn.classList.add('hide');
-    rollBtn.classList.add('hide');
-};
-
-
-setScorebtn.addEventListener('click', function(){
-    inputValue = input.value;
-    holdBtn.classList.remove('hide');
-    rollBtn.classList.remove('hide');
-    document.getElementById('player-' + player).classList.remove('winner');
-    
-});
-    
-    
-})();
+    });
+        })();
